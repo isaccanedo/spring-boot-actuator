547b814ab86d86d64850add034211283a1314dff
isaccanedo <isaccanedo@gmail.com>
2020-08-22 20:46:14 -0300

    First commit

---
+++ src/main/java/com/isaccanedo/health/actuatorexample/CustomEndPoint.java
@@ -0,0 +1,27 @@
+package com.isaccanedo.health.actuatorexample;
+
+import org.springframework.boot.actuate.endpoint.Endpoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomEndPoint implements Endpoint<String>{
+    @Override
+    public String getId() {
+        return "Isac Canedo";
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isSensitive() {
+        return false;
+    }
+
+    @Override
+    public String invoke() {
+        return "Hello Isac Canedo";
+    }
+}
